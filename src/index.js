53207980493ea74cb3038860bb8842a46d0b8c80
Pavel Lichmanuyk <pavel.lichmanuyk@gmail.com>
2018-10-06 13:23:59 +0300

    Finish

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.arr = [];
+    this.comparator = (a, b) => a - b;
   }
 
   add(element) {
-    // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.arr[index];
   }
 
   get length() {
-    // your implementation
+    return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+    return this.arr;
   }
 
   sort(indices) {
-    // your implementation
+    const sortedArr = [];
+    const sortedIndices = indices.sort((a, b) => a - b);
+
+    for (let i = 0, length = sortedIndices.length; i < length; i++) {
+      sortedArr.push(this.arr[sortedIndices[i]]);
+    }
+
+    sortedArr.sort[this.comparator];
+
+    for (let i = 0, length = sortedIndices.length; i < length; i++) {
+      this.arr[sortedIndices[i]] = sortedArr.shift();
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
   }
 }
 
